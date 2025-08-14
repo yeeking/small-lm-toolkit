10ac522ccf80f11c4d91f89fed150cb838538468
MYK <matthew@teafortwo>
2025-08-14 20:16:17 +0100

    rebuilt validator dataset and tuned training dataset modules

---
+++ src/test_validation.py
@@ -0,0 +1,15 @@
+import shared_utils
+from pathlib import Path
+
+
+# files = shared_utils.find_text_files(Path('data/tiny_dataset/training/'))
+files = shared_utils.find_text_files(Path('data/example_datafiles/'))
+print(f"Retrieved {len(files)} files.")
+
+# files = ["val/file1.txt",     "val/file2.txt"]
+ds = shared_utils.MiddleP2WindowDataset(files, min_p2=4, max_p2=256)
+print(f"Retrieved {len(ds)} data points.")
+print(f"Token counts : {[len(d) for d in ds]}")
+# print(f"HEre is the first one,{ds[0]}")
+
+      
