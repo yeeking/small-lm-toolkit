57f328f04a9f4e9c5cd7d870839ddb536a8a305b
Matthew Yee-King <matthew@yeeking.net>
2025-08-15 16:44:33 +0100

    adding slurm scripts

---
+++ scripts/slurm_small_lm_finetune.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH --time 10080          # time in minutes to reserve
+#SBATCH --cpus-per-task 8  # number of cpu cores
+#SBATCH --mem 32G           # memory pool for all cores
+#SBATCH --gres gpu:nvidia_a30:1       # number of gpu cores
+#SBATCH --nodelist ben  # bill be broke for now
+#SBATCH  -o small-lm-finetune-test.log      # write output to log file
+
+srun   -l  python ../../src/small-lm-toolkit/src/6_run_finetunes.py
+
