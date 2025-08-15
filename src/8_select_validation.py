4d2e30542446e96e091ea5085a727ed7cc28aa8c
Matthew on office mac studio <myk@macstudio.office>
2025-08-15 15:07:35 +0100

    quite hacky validation dataset prep script

--- src/8_select_validation.py
+++ src/8_select_validation.py
@@ -19,12 +19,15 @@ def get_base_name(filepath):
     return name
 
 def main():
-    folder = "/Users/matthewyk/src/ai-music/data/midi/"
-    out_folder = "/Users/matthewyk/src/ai-music/data/10_piano_solos/"
+    # folder = "/Users/matthewyk/src/ai-music/data/midi/"
+    # out_folder = "/Users/matthewyk/src/ai-music/data/10_piano_solos/"
+    folder = "/Users/matthewyk/src/ai-music/data/small-lm-datasets/midi_hawthorne_all_keys/njam/"
+    out_folder = "/Users/matthewyk/src/ai-music/data/validation_dataset/"
     assert os.path.exists(folder)
     assert os.path.exists(out_folder)
 
-    files = shared_utils.find_files(Path(folder), '.midi')
+    files = shared_utils.find_files(Path(folder), '.txt')
+    files = [f for f in files if "plus_0" in str(f)]
     assert len(files) > 0, f"Failed to find midi in {folder}"
     print(f"Found {len(files)} midi files")
     files.sort(key=lambda x: os.path.basename(x))
@@ -37,14 +40,28 @@ def main():
     
     # Print counts and original paths, sorted by count descending
     sorted_items = sorted(name_to_paths.items(), key=lambda x: len(x[1]), reverse=False)
-    for clean_name, paths in sorted_items[-20:]:
-        print(f"{clean_name}")
-        
-        # Sort paths by file size and take the smallest one
-        src_path = sorted(paths, key=lambda x: os.path.getsize(x))[0]
-        dst_path = os.path.join(out_folder, os.path.basename(src_path))
-        shutil.copy2(src_path, dst_path)
-        print(f"Copied {src_path} ({os.path.getsize(src_path)} bytes) to {dst_path}")
+    with open(os.path.join(out_folder, 'file_list.txt'), 'w') as f:
+        players = []
+        for clean_name, paths in sorted_items[-20:]:
+            print(f"{clean_name} {len(paths)}")
+            
+            # Sort paths by file size and take the smallest one
+            paths_by_size = sorted(paths, key=lambda x: os.path.getsize(x))
+            for ind,p in enumerate(paths_by_size):
+                player = str(p).split('/')[-4]
+                if player in players: 
+                    print(f"Seen player {player}")
+                    continue
+                print(player)
+                src_path = paths_by_size[ind]
+    
+            print(f"Chose player {player}")
+
+            players.append(player)
+            dst_path = os.path.join(out_folder, os.path.basename(src_path))
+            shutil.copy2(src_path, dst_path)
+            print(f"Copied {src_path} ({os.path.getsize(src_path)} bytes) to {dst_path}")
+            f.write(str(src_path) + "\n")
 
 if __name__ == "__main__":
     main()
